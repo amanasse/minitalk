a667b390ee4d50845f1b4aea4d76ad43719ef810
Adrian MANASSERO <amanasse@e3r12p13.clusters.42paris.fr>
2022-08-10 17:54:17 +0200

    ajouts mineurs

--- main_client.c
+++ main_client.c
@@ -6,7 +6,7 @@
 /*   By: amanasse <amanasse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/04 15:34:16 by amanasse          #+#    #+#             */
-/*   Updated: 2022/08/10 15:39:29 by amanasse         ###   ########.fr       */
+/*   Updated: 2022/08/10 17:53:20 by amanasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,29 @@ int main (int argc, char **argv)
     printf("client OK :\n");
     (void)argc;
 
-    
-    kill(atoi(argv[1]), SIGUSR1);
-    kill(atoi(argv[1]), SIGUSR2);
-
+    if (argc >= 2)
+    {
+    	kill(atoi(argv[1]), SIGUSR2);
+		usleep(90);
+    	kill(atoi(argv[1]), SIGUSR1);
+		usleep(90);
+    	kill(atoi(argv[1]), SIGUSR2);
+		usleep(90);
+    	kill(atoi(argv[1]), SIGUSR1);
+		usleep(90);
+    	kill(atoi(argv[1]), SIGUSR1);
+		usleep(90);
+    	kill(atoi(argv[1]), SIGUSR1);
+		usleep(90);
+    	kill(atoi(argv[1]), SIGUSR1);
+		usleep(90);
+    	kill(atoi(argv[1]), SIGUSR1);
+		usleep(90);
+    	kill(atoi(argv[1]), SIGUSR1);
+		usleep(90);
+    	kill(atoi(argv[1]), SIGUSR1);
+		usleep(90);
+    	kill(atoi(argv[1]), SIGUSR1);
+    }
     return (0);
 }
--- main_serveur.c
+++ main_serveur.c
@@ -6,37 +6,41 @@
 /*   By: amanasse <amanasse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/08 12:35:39 by amanasse          #+#    #+#             */
-/*   Updated: 2022/08/10 15:39:31 by amanasse         ###   ########.fr       */
+/*   Updated: 2022/08/10 17:53:19 by amanasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void    listen(int sig, siginfo_t *info, void *tmp)
+void    listen(int sig)
 {
-    (void)tmp;
-    (void)info;
     if (sig == SIGUSR1)
-        write(1, "ft_stock_1\n", 5);
+        write(1, "ft_stock_1\n", 11);
     else if (sig == SIGUSR2)
-        write(1, "ft_stock_0\n", 5);
-    else
-        write(1, "error\n", 6);
+        write(1, "ft_stock_0\n", 11);
+    else 
+    {
+        write(1, "exit\n", 5);
+        exit(0);
+    }
 }
+
 int main ()
 {
     struct sigaction sa;
 
-
-    sa.sa_sigaction = &listen;
+    // SA_SIGACTION POUR RECUP LE SIGNAL DE LEMETTEUR
+    sa.sa_handler = &listen;
+    sa.sa_flags = SA_RESTART;
     printf("serveur OK : %d\n",getpid());
     sleep(1);
-
     sigaction(SIGUSR1, &sa, NULL);
     sigaction(SIGUSR2, &sa, NULL);
-    printf("listen \n");
+    sigaction(SIGINT, &sa, NULL);
+    
     while (1)
-        pause();
-    printf("serveur KO :\n");
+    {
+        signal = pause;
+    }
     return (0);
 }
